560e97e0c09b20d2d469cda2b086a8297b6d2833
mwasif0005-bit <mwasif0005@gmail.com>
2026-04-25 15:51:37 +0500

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,18 @@
+public class Book {
+    int id;
+    String title;
+    String author;
+    boolean isAvailable;
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+    }
+
+    public void display() {
+        System.out.println(id + " | " + title + " | " + author + " | " + 
+                          (isAvailable ? "Available" : "Issued"));
+    }
+}
